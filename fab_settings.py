89076fd999ff68f8d25bfc512ba6638da69d01e4
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-06-02 16:26:53 +0200

    settings

---
+++ fab_settings.py
@@ -0,0 +1,48 @@
+extra_paths = ':'.join([
+    '/home/%(user)s/%(project_name)s/local_apps',
+    '/home/%(user)s/%(project_name)s/project',
+    '/home/%(user)s/%(project_name)s/project/%(project_name)s'
+])
+global_settings = [
+    ('postgres_logging', False),
+    ('django_debug', False),
+    ('project_name', 'simple'),
+    ('db_user', 'simple'),
+    ('db_password', 'example123'),
+    ('db_host', 'localhost'),
+    ('use_nginx', 'true'),
+    ('nginx_user', 'www-data'),
+    ('nginx_confdir', '/etc/nginx/'),
+    ('nginx_pidfile', '/var/run/nginx.pid'),
+    ('nginx_bin', '/usr/sbin/nginx'),
+    ('gunicorn_host', '127.0.0.1'),
+    ('extra_settings', ''),
+    ('staticroot', '/home/%(user)s/static'),
+    ('mediaroot', '/home/%(user)s/static/media'),
+    ('pg_dump', '/usr/bin/pg_dump'),
+    ('pg_dropdb', '/usr/bin/dropdb'),
+    ('psql', '/usr/bin/psql'),
+    ('postgres_user', 'postgres'),
+    ('root', '/home/%(user)s/%(project_name)s'),
+    ('extra_paths', extra_paths)
+]
+
+group_settings = {
+
+    'vagrantdevelop': {
+        'servername': 'simple.com',
+        'gunicorn_port': '8001',
+        'gunicorn_autostart': 'true',
+        'db_name': 'simple_develop',
+        'memcached_port': '11212',
+        'solr_cores': 'develop'
+    }
+    
+}
+
+servers = {
+#   'role', [
+#       ['me@server:port', 'settings_group']
+#   ],
+    'vagrant': [['vagrant@127.0.0.1:22', 'vagrantdevelop']],
+}
